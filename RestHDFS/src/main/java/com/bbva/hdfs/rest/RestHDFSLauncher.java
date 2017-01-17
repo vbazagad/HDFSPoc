6cd71ac225cf5ff4c4386d4d20aa85d84bbe6442
Victor B <victor.bazaga@gmail.com>
2017-01-17 11:59:39 +0100

    Update RestHDFSLauncher.java
    Agregar imports faltantes

--- RestHDFS/src/main/java/com/bbva/hdfs/rest/RestHDFSLauncher.java
+++ RestHDFS/src/main/java/com/bbva/hdfs/rest/RestHDFSLauncher.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RestHDFSLauncher extends SpringBootServletInitializer {
